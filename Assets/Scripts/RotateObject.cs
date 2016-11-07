e4fc6166708ccb3d31fb43e165074248fb2b92bc
Erik Slowikowski <e.slowikowski@bigpoint.net>
2016-11-07 15:20:07 +0100

    Rotating cube in black room

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateObject : MonoBehaviour {
+
+    public float RotSpeed = 20;
+
+    void OnMouseDrag()
+    {
+        float rotX = Input.GetAxis("Mouse X") * RotSpeed * Mathf.Deg2Rad;
+        float rotY = Input.GetAxis("Mouse Y") * RotSpeed * Mathf.Deg2Rad;
+
+        transform.RotateAround(Vector3.up, -rotX);
+        transform.RotateAround(Vector3.right, rotY);
+    }
+}
