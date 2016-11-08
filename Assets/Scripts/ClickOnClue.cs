d9ac78b930f2f151ae66827b6e37cc9224173736
Erik Slowikowski <e.slowikowski@bigpoint.net>
2016-11-08 11:53:06 +0100

    added UI

---
+++ Assets/Scripts/ClickOnClue.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickOnClue : MonoBehaviour {
+
+    private RaycastHit _hit;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out _hit))
+            {
+                if (_hit.transform.tag.Contains("Clue"))
+                {
+                    this.GetComponent<CluesUIScript>().UpdateScore();
+                    print("TestHit");
+                    _hit.collider.gameObject.SetActive(false);
+                }
+
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/CluesUIScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CluesUIScript : MonoBehaviour {
+    public Text Points;
+
+    private int Score = 0;
+
+    public void UpdateScore()
+    {
+        Score++;
+        Points.text = Score+"/5";
+    }
+}
