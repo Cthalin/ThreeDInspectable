ae2a851a65f0eaf66f9323ad481eb6a9895b2d74
Erik Slowikowski <e.slowikowski@bigpoint.net>
2016-11-08 15:45:19 +0100

    status 08/11

--- Assets/Scripts/ClickOnClue.cs
+++ Assets/Scripts/ClickOnClue.cs
@@ -4,21 +4,60 @@ using System.Collections;
 public class ClickOnClue : MonoBehaviour {
 
     public LayerMask layerMask;
+    public GameObject Ring;
     private RaycastHit _hit;
+
+    void Start()
+    {
+        Ring.SetActive(false);
+    }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            print(layerMask.value);
-            if (Physics.Raycast(ray, out _hit, 1000f, layerMask))
-            {
-                this.GetComponent<CluesUIScript>().UpdateScore();
-                print("TestHit "+GetComponent<CluesUIScript>().Points.text);
-                _hit.collider.gameObject.SetActive(false);
-            }
+        //    if (Physics.Raycast(ray, out _hit, 1000f, layerMask))
+        //    {
+        //        this.GetComponent<CluesUIScript>().UpdateScore();
+        //        //print("TestHit "+GetComponent<CluesUIScript>().Points.text);
+        //        var pos = Camera.main.WorldToScreenPoint(_hit.transform.position);
+
+        //        print(pos);
+
+        //        Ring.SetActive(true);
+        //        Ring.transform.position = pos;
+        //        StartCoroutine(VanishRing(Ring));
+                
+        //        _hit.collider.gameObject.SetActive(false);
+        //    }
+        //}
+    }
+
+    void OnMouseDown()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out _hit, 1000f, layerMask))
+        {
+            this.GetComponent<CluesUIScript>().UpdateScore();
+            //print("TestHit "+GetComponent<CluesUIScript>().Points.text);
+            var pos = Camera.main.WorldToScreenPoint(_hit.transform.position);
+
+            print(pos);
+
+            Ring.SetActive(true);
+            Ring.transform.position = pos;
+            StartCoroutine(VanishRing(Ring));
+
+            _hit.collider.gameObject.SetActive(false);
         }
+    }
+
+    IEnumerator VanishRing(GameObject Ring)
+    {
+        yield return new WaitForSeconds(2f);
+        Ring.SetActive(false);
     }
 }
