5abf5eeaf34283417251c37d4b1790f9205a05c9
Erik Slowikowski <e.slowikowski@bigpoint.net>
2016-11-08 09:52:51 +0100

    added head and press card

---
+++ Assets/Scripts/StartButtonScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartButtonScript : MonoBehaviour {
+
+    public void StartTheGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
