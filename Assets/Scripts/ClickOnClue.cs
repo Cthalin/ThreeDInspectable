3cc059c6402e47f9210bf45b8091aab9bf0c90a3
Erik Slowikowski <e.slowikowski@bigpoint.net>
2016-11-08 14:49:50 +0100

    added layermask for clues

--- Assets/Scripts/ClickOnClue.cs
+++ Assets/Scripts/ClickOnClue.cs
@@ -11,33 +11,13 @@ public class ClickOnClue : MonoBehaviour {
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit[] hits;
-            hits = Physics.RaycastAll(ray, 100);
 
-            //for (int i = 0; i < hits.Length; i++)
-            //{
-            //    RaycastHit hit = hits[i];
-                
-            //    if (hits[i].transform.tag.Contains("Clue"))
-            //    {
-            //        this.GetComponent<CluesUIScript>().UpdateScore();
-            //        print("TestHit " + GetComponent<CluesUIScript>().Points.text);
-            //        hits[i].collider.gameObject.SetActive(false);
-            //        break;
-            //    }else
-            //    {
-            //        break;
-            //    }
-            //}
-
-            if (Physics.Raycast(ray, out _hit, layerMask))
+            print(layerMask.value);
+            if (Physics.Raycast(ray, out _hit, 1000f, layerMask))
             {
-                if (_hit.transform.tag.Contains("Clue"))
-                {
-                    this.GetComponent<CluesUIScript>().UpdateScore();
-                    print("TestHit "+GetComponent<CluesUIScript>().Points.text);
-                    _hit.collider.gameObject.SetActive(false);
-                }
+                this.GetComponent<CluesUIScript>().UpdateScore();
+                print("TestHit "+GetComponent<CluesUIScript>().Points.text);
+                _hit.collider.gameObject.SetActive(false);
             }
         }
     }
