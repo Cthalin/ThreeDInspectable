a038fad5ecf1124942f7283da49f697b6894a171
Erik Slowikowski <eslowikowski@bigpoint.net>
2016-11-15 19:26:23 +0100

    added cube and 1-3 sides

---
+++ Assets/Scripts/CubeStart.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeStart : MonoBehaviour {
+
+    public GameObject[] NumberPlanes = new GameObject[3];
+    private float _random;
+    private int _randomSide;
+
+    void Awake()
+    {
+        for(int i=0; i<NumberPlanes.Length; i++)
+        {
+            NumberPlanes[i].SetActive(false);
+        }
+    }
+    void Start () {
+        _random = Random.Range(0f, 2f);
+        _randomSide = (int) Mathf.Round(_random);
+        NumberPlanes[_randomSide].SetActive(true);
+	}
+}
