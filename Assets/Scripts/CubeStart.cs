bf8f08df6a604189afe2e0fa7d5553ef00ba4fe4
Erik Slowikowski <eslowikowski@bigpoint.net>
2016-11-15 20:09:53 +0100

    added planes

--- Assets/Scripts/CubeStart.cs
+++ Assets/Scripts/CubeStart.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class CubeStart : MonoBehaviour {
 
-    public GameObject[] NumberPlanes = new GameObject[3];
+    public GameObject[] NumberPlanes = new GameObject[4];
     private float _random;
     private int _randomSide;
 
@@ -15,7 +15,7 @@ public class CubeStart : MonoBehaviour {
         }
     }
     void Start () {
-        _random = Random.Range(0f, 2f);
+        _random = Random.Range(0f, 3f);
         _randomSide = (int) Mathf.Round(_random);
         NumberPlanes[_randomSide].SetActive(true);
 	}
